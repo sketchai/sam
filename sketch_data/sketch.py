c8a0add46d329b219367ee21bc87e105d4018bb1
Frederique ROBIN <frederique.robin@edf.fr>
2022-03-25 17:06:21 +0100

    add sketch

---
+++ sketch_data/sketch.py
@@ -0,0 +1,55 @@
+from typing import List
+import pickle
+import matplotlib.pyplot as plt
+
+from sketch_data.primitive import Primitive
+from sketch_data.constraint import Constraint
+
+
+class Sketch:
+    def __init__(self):
+        self.sequence: List[Int] = []
+
+    def add(self, elt: object):
+        self.sequence.append(elt)
+
+    def _prepare_draw(self, ax=None, show_axes: bool = True):
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, aspect='equal')
+        else:
+            fig = None
+
+        # Eliminate upper and right axes
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
+
+        if not show_axes:
+            ax.set_yticklabels([])
+            ax.set_xticklabels([])
+            _ = [line.set_marker('None') for line in ax.get_xticklines()]
+            _ = [line.set_marker('None') for line in ax.get_yticklines()]
+
+            # Eliminate lower and left axes
+            ax.spines['left'].set_color('none')
+            ax.spines['bottom'].set_color('none')
+
+        return fig, ax
+
+    def draw(self, show_axes: bool = True):
+
+        fig, ax = self._prepare_draw()
+        for s in self.sequence:
+            if isinstance(s, Primitive):
+                p_fn = s.plot(ax)
+                if p_fn is None:
+                    continue
+
+        # Rescale axis limits
+        ax.relim()
+        ax.autoscale_view()
+
+        return fig
+
+    def export(self, out_path: str):
+        pickle.dump({'seq': self.sequence}, open(out_path, "wb"))
---
+++ tests/unit/test_sketch.py
@@ -0,0 +1,40 @@
+from sketch_data.catalog_primitive import Circle, Line, Arc
+from sketch_data.sketch import Sketch
+import unittest
+import logging
+import matplotlib.pyplot as plt
+import pickle
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+class TestSketch(unittest.TestCase):
+
+    def test_init(self):
+
+        # construction
+        sketch = Sketch()
+
+        sketch.add(Circle(center=[0., 5.], radius=1))
+        sketch.add(Line(pnt1_X=0.2, pnt1_Y=0.2, pnt2_X=0.3, pnt2_Y=0.7))
+
+    # def test_show(self):
+    #     sketch = Sketch()
+
+    #     sketch.add(Circle(center=[0.,5.], radius=1))
+    #     sketch.add(Arc(center=[0.,5.], radius=1, angles = [90.,180.]))
+    #     sketch.add(Line(pnt1_X =0.2, pnt1_Y = 0.2, pnt2_X = 0.3, pnt2_Y=0.7))
+
+    #     fig = sketch.draw()
+    #     plt.show()
+
+    def test_export(self):
+        sketch = Sketch()
+        sketch.add(Circle(center=[0., 5.], radius=1))
+
+        out_path = 'tests/unit/asset/out/sketch.pkl'
+        sketch.export(out_path=out_path)
+
+        seq = pickle.load(open(out_path, "rb"))
+        logger.info(f'seq: {seq}')
