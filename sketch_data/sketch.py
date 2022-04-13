dc968dd6b2df2e26f0788b3009231913a5d195d3
Frederique ROBIN <frederique.robin@edf.fr>
2022-04-13 14:22:52 +0200

    clean shaper

--- sketch_data/sketch.py
+++ sketch_data/sketch.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 from sketch_data.primitive import Primitive
 from sketch_data.constraint import Constraint
 
-sys.path.append("/home/H03832/Donnees/GAN_CAO/gitlab_pleiade/SketchGraphs_For_EDF/sketchgraphs")
-from sketchgraphs.data import flat_array
-import sketchgraphs.data as datalib
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
 
 class Sketch:
     def __init__(self):
-        self.sequence: List[Int] = []
+        self.sequence: List = []
 
     def add(self, elt: object):
         self.sequence.append(elt)
@@ -54,31 +54,6 @@ class Sketch:
 
         return fig
 
-    def read_random_sequence_from_Sketchgraphs_file(self, dataset = 'sg_t16_validation.npy'):
-        # 'sg_t16_train.npy'
-        import sys, random, logging
-        logging.basicConfig(level=logging.DEBUG)
-        logger = logging.getLogger()
-
-        data_path = "/home/H03832/Donnees/GAN_CAO/gitlab/data_sous_Marc/"
-        seq_data = flat_array.load_dictionary_flat(data_path+dataset)
-        print("*********************************************************************")
-        print(len(seq_data['sequences']), "séquences")
-        i = random.randint(0,len(seq_data['sequences']))
-        print("index séquence :", i)
-        seq = seq_data['sequences'][i]
-        print("Longueur de la séquence =", len(seq))
-        cpt = 0
-        for _, s in enumerate(seq):
-            if 'NodeOp' in str(s):
-                # print(f'index= {cpt}, s={s}')
-                logger.info(f'index= {cpt}, s={s}')
-                cpt += 1
-            else :
-                # print(f'........... s= {s}')
-                logger.info(f'........... s= {s}')
-        # sketch = datalib.sketch_from_sequence(seq)
-        # datalib.render_sketch(sketch, show_axes=True, show_origin=True);
-
     def export(self, out_path: str):
+        logger.info(f'save in {out_path}')
         pickle.dump({'seq': self.sequence}, open(out_path, "wb"))
