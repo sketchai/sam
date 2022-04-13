1f1476e2fa16a09cc04b88ce53927fd0dac6a178
Raphael MARC <H03832@dsp0961748>
2022-04-13 12:30:07 +0200

    Ajout contraintes manquantes

--- sketch_data/catalog_constraint/geometric_constraint.py
+++ sketch_data/catalog_constraint/geometric_constraint.py
@@ -6,6 +6,11 @@ class Coincident(Constraint):
     def __init__(self, references: List = []):
         super(Coincident, self).__init__(elt_type=ConstraintType.COINCIDENT, references=references)
 
+class Equal(Constraint):
+    """Equal Constraint (same length for lines or same radius for circles or arcs"""
+
+    def __init__(self, references: List = []):
+        super(Equal, self).__init__(elt_type=ConstraintType.EQUAL, references=references)
 
 class Horizontal(Constraint):
     """Horizontal constraint"""
@@ -13,12 +18,22 @@ class Horizontal(Constraint):
     def __init__(self, references: List = []):
         super(Horizontal, self).__init__(elt_type=ConstraintType.HORIZONTAL, references=references)
 
+class Midpoint(Constraint):
+
+    def __init__(self, references: List = []):
+        super(Midpoint, self).__init__(elt_type=ConstraintType.MIDPOINT, references=references)
+
 class Vertical(Constraint):
     """Vertical constraint"""
 
     def __init__(self, references: List = []):
         super(Vertical, self).__init__(elt_type=ConstraintType.VERTICAL, references=references)
 
+class Tangent(Constraint):
+    """Tangent Constraint"""
+
+    def __init__(self, references: List = []):
+        super(Tangent, self).__init__(elt_type=ConstraintType.TANGENT, references=references)
 
 class Parallel(Constraint):
     """Parallel Constraint"""
@@ -27,7 +42,7 @@ class Parallel(Constraint):
         super(Parallel, self).__init__(elt_type=ConstraintType.PARALLEL, references=references)
 
 class Perpendicular(Constraint):
-    """Parallel Constraint"""
+    """Perpendicular Constraint"""
 
     def __init__(self, references: List = []):
         super(Perpendicular, self).__init__(elt_type=ConstraintType.PERPENDICULAR, references=references)
